4b820ad5269fe8945e559fe3ee5e210b3e2686d6
Willian Silvestre <70384084+WillianSi@users.noreply.github.com>
2025-05-07 12:52:47 -0300

    1.0

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwAJ7WuqpMHe7-dQeRzgcA-XIuaurFJkE",
-  authDomain: "farmgest-176d3.firebaseapp.com",
-  projectId: "farmgest-176d3",
-  storageBucket: "farmgest-176d3.appspot.com",
-  messagingSenderId: "441761572298",
-  appId: "1:441761572298:web:a4b49d864c121bd5819b33",
-  measurementId: "G-YSN9X2MMX1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
